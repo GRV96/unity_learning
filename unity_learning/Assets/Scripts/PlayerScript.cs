69692998a3e21fa6571011e4d97c34e2f1550044
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-06-24 13:24:34 -0400

    Player position is reset when the game begins.

--- unity_learning/Assets/Scripts/PlayerScript.cs
+++ unity_learning/Assets/Scripts/PlayerScript.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-	// Start is called before the first frame update
+	// Start is called before the first frame update.
 	void Start() {
-		// Rien
+		transform.SetPositionAndRotation(
+			new Vector3(0.0f, 1.4f, -14.0f),
+			new Quaternion(0.0f, 0.0f, 0.0f, 1));
 	}
 
-	// FixedUpdate is called once per frame
+	// FixedUpdate is called once per frame.
 	void FixedUpdate() {
-		transform.Translate(Vector3.forward * 0.3f * Input.GetAxis("Vertical"));
-		transform.Translate(Vector3.right * 0.2f * Input.GetAxis("Horizontal"));
+		transform.Translate(
+			Vector3.forward * 0.3f * Input.GetAxis("Vertical"));
+		transform.Translate(
+			Vector3.right * 0.2f * Input.GetAxis("Horizontal"));
 	}
 }
