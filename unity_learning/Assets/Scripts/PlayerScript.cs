39e278dcf60d393b22c839c1cc61294932e5658e
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2023-06-24 17:33:05 -0400

    Attempt at using Rigidbody.MovePosition

--- unity_learning/Assets/Scripts/PlayerScript.cs
+++ unity_learning/Assets/Scripts/PlayerScript.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+	private Rigidbody rigidbody = null;
+
 	// Start is called before the first frame update.
 	void Start() {
+		rigidbody = GetComponent<Rigidbody>();
 		transform.SetPositionAndRotation(
 			new Vector3(0.0f, 1.4f, -14.0f),
 			new Quaternion(0.0f, 0.0f, 0.0f, 1));
@@ -13,15 +16,15 @@ public class PlayerScript : MonoBehaviour
 
 	// FixedUpdate is called once per frame.
 	void FixedUpdate() {
-		float zSpeed = 0.3f * Input.GetAxis("Vertical");
-		float xSpeed = 0.2f * Input.GetAxis("Horizontal");
-		//*
-		transform.Translate(Vector3.forward * zSpeed);
-		transform.Translate(Vector3.right * xSpeed);
-		//*/
+		float deltaX = 0.2f * Input.GetAxis("Horizontal");
+		float deltaZ = 0.3f * Input.GetAxis("Vertical");
 		/*
-		rigidbody.velocity.z = zSpeed;
-		rigidbody.velocity.x = xSpeed;
+		transform.Translate(Vector3.right * deltaX);
+		transform.Translate(Vector3.forward * deltaZ);
+		//*/
+		//*
+		rigidbody.MovePosition(
+			rigidbody.position + new Vector3(deltaX, 0f, deltaZ));
 		//*/
 	}
 }
